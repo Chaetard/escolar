c0b5ab38212a74f3333ee781ee547055159d89b8
Chaetard <jesus.santos4160@alumnos.udg.mx>
2023-09-11 15:10:57 -0600

    integracion del horario

--- Js/panelListas.js
+++ Js/panelListas.js
@@ -4,7 +4,7 @@ const alumnosL = alumnos.map((clase) => {
 
 const alumnosAdd = document.getElementById("alumnosAdd");
 
-function alumnoBuscado(tamaño) {
+function alumnoBuscado(tamaño, palabraClave) {
   for (let i = 0; i < tamaño; i++) {
     const divmore = document.createElement("div");
     divmore.innerHTML = `
@@ -17,13 +17,60 @@ function alumnoBuscado(tamaño) {
   </tr>
   </table>
   `;
-  alumnosAdd.appendChild(divmore);
+    alumnosAdd.appendChild(divmore);
   }
-
-  
 }
 
 alumnoBuscado(alumnosL.length);
+const busqueda = document.getElementById("busqueda");
+
+busqueda.addEventListener("keydown", (e) => {
+
+  let busqueda = e.target.value;
+  console.log(busqueda);
+
+  alumnosAdd.innerHTML = `${busqueda}`;
+  
+});
 
 
 
+
+
+
+// CHAT GPT CODE 
+
+/* 
+
+// Obtenemos el campo de entrada y la lista
+const searchInput = document.getElementById('searchInput');
+const lista = document.getElementById('lista');
+
+// Agregamos un evento 'keypress' al campo de entrada
+searchInput.addEventListener('keypress', function () {
+  // Obtenemos el valor del campo de entrada
+  const searchTerm = searchInput.value.toLowerCase();
+
+  // Obtenemos todos los elementos de la lista
+  const elementos = lista.getElementsByTagName('li');
+
+  // Recorremos los elementos de la lista
+  for (let i = 0; i < elementos.length; i++) {
+    const elemento = elementos[i];
+    const textoElemento = elemento.innerText.toLowerCase();
+
+    // Comparamos el texto del elemento con el término de búsqueda
+    if (textoElemento.includes(searchTerm)) {
+      // Mostramos el elemento si coincide con la búsqueda
+      elemento.style.display = 'block';
+    } else {
+      // Ocultamos el elemento si no coincide con la búsqueda
+      elemento.style.display = 'none';
+    }
+  }
+});
+
+
+
+
+*/
