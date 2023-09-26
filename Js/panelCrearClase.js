ed39e094ef184e7e2eb4515c8f31c1cd8c4c5813
Chaetard <jesus.santos4160@alumnos.udg.mx>
2023-09-26 12:49:09 -0600

    terminando seleccion de iconos y dias

--- Js/panelCrearClase.js
+++ Js/panelCrearClase.js
@@ -1,4 +1,4 @@
-const img = imgs.map((img) => { 
+const img = imgs.map((img) => {
   return img;
 });
 
@@ -21,3 +21,57 @@ color_tarjetaListener.addEventListener("input", () => {
   const color_tarjeta = document.getElementById("color_tarjeta").value;
   visuaCard.style.backgroundColor = color_tarjeta;
 });
+
+const iconos = document.getElementById("iconos");
+const elementos = document.getElementsByClassName("imagenes");
+const logo = document.getElementById("logo");
+
+for (let i = 0; i < img.length; i++) {
+  iconos.innerHTML += `<img src="${img[i].img}" alt="" class="imagenes">`;
+}
+
+for (let i = 0; i < elementos.length; i++) {
+  elementos[i].addEventListener("click", () => {
+    limpiarFondo();
+    logo.src = elementos[i].src;
+    console.log(elementos[i].src);
+    elementos[i].style.backgroundColor = "rgba(5, 173, 5, 0.835)";
+  });
+}
+
+function limpiarFondo() {
+  for (let i = 0; i < elementos.length; i++) {
+    elementos[i].style.backgroundColor = "";
+  }
+}
+
+const hora = document.getElementById("hora");
+const horavista = document.getElementById("horaselect");
+
+hora.addEventListener("input", () => {
+  const hora = document.getElementById("hora").value;
+  console.log(hora);
+  horaselect.innerText = hora;
+});
+
+const contenedordias = document.getElementById("contenedordias");
+const checkboxes = document.getElementsByClassName("dias");
+
+for (var i = 0; i < checkboxes.length; i++) {
+  // Captura el valor de 'i' en cada iteración
+  (function (index) {
+    checkboxes[index].addEventListener("change", function () {
+      if (this.checked) {
+        contenedordias.innerHTML += `<span  class="${checkboxes[index].id}" >   ${checkboxes[index].name}</span>`;
+        console.log(checkboxes[index].id);
+      } else {
+        let elemento = document.getElementsByClassName(checkboxes[index].id);
+        
+        elemento[0].remove();
+        
+      }
+    });
+  })(i);
+}
+
+
